1a9e080c41452cdcd27b19d0d149b7ff38359119
xlSosdee <glimascapillo@gmail.com>
2025-03-10 02:20:05 -0500

    intento 5 casi

--- consumer.py
+++ consumer.py
@@ -2,12 +2,20 @@ from kafka import KafkaConsumer
 import json
 import os
 
-# Conectar a Kafka
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
+# Conectar a Kafka usando SSL
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9093")
+# Archivos correctos según tu directorio ./certs
+SSL_CERT_PATH = "/certs/kafka-cert.pem"  # Certificado del cliente
+SSL_KEY_PATH = "/certs/kafka-key-credentials"  # Archivo que podría contener la clave
+SSL_CA_PATH = "/certs/kafka.server.truststore.jks"  # Posible autoridad certificadora (CA)
 
 consumer = KafkaConsumer(
     "synthetic_data",
     bootstrap_servers=KAFKA_BROKER,
+    security_protocol="SSL",
+    ssl_cafile=SSL_CA_PATH,
+    ssl_certfile=SSL_CERT_PATH,
+    ssl_keyfile=SSL_KEY_PATH,
     auto_offset_reset="earliest",
     enable_auto_commit=True,
     group_id='my-group',  # Añadir el group_id
--- producer.py
+++ producer.py
@@ -6,11 +6,19 @@ import time
 from datetime import datetime
 import os
 
-# Conectar a Kafka
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
+# Conectar a Kafka usando SSL
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9093")
+# Archivos correctos según tu directorio ./certs
+SSL_CERT_PATH = "/certs/kafka-cert.pem"  # Certificado del cliente
+SSL_KEY_PATH = "/certs/kafka-key-credentials"  # Archivo que podría contener la clave
+SSL_CA_PATH = "/certs/kafka.server.truststore.jks"  # Posible autoridad certificadora (CA)
 
 producer = KafkaProducer(
     bootstrap_servers=KAFKA_BROKER,
+    security_protocol="SSL",
+    ssl_cafile=SSL_CA_PATH,
+    ssl_certfile=SSL_CERT_PATH,
+    ssl_keyfile=SSL_KEY_PATH,
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
